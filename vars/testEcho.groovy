0fa1bffb09ef2d35e7e42249a277e2617ced7e23
Andres Lopez <andres.lp.dev@gmail.com>
2024-06-17 12:45:19 -0500

    update testEcho file - duplicate 'pipeline'

--- vars/testEcho.groovy
+++ vars/testEcho.groovy
@@ -1,15 +1,3 @@
 def call(Map params) {
-    pipeline {
-        agent any
-
-        stages {
-            stage('test echo') {
-                steps {
-                    script {
-                        sh "echo Hola ${params.name}"
-                    }
-                }
-            }
-        }
-    }
+  sh "echo Hola ${params.name}"
 }
