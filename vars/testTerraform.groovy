3f279e08f2ea1f5b53ee130a32e996ba9914da1d
Andres Lopez <andres.lp.dev@gmail.com>
2024-06-17 14:44:11 -0500

    test pipeline params

--- vars/testTerraform.groovy
+++ vars/testTerraform.groovy
@@ -1,19 +1,13 @@
 def call(Map params) {
-    stage('variables') {
-        steps {
-            script {
-                env.REGION = "${params.region}"
-                def region = params.region ?: env.REGION
-                env.INSTANCE_TYPE = "${params.instance_type}"
-                def instance_type = params.instance_type ?: env.INSTANCE_TYPE
+    env.REGION = "${params.region}"
+    def region = params.region ?: env.REGION
+    env.INSTANCE_TYPE = "${params.instance_type}"
+    def instance_type = params.instance_type ?: env.INSTANCE_TYPE
 
-                if (region == null || instance_type == null){
-                    sh "error Error con la variable region"
-                }else{
-                    sh "echo Region selecionada: $REGION"
-                    sh "echo Tipo de instancia selecionada: $INSTANCE_TYPE"
-                }
-            }
-        }
+    if (region == null || instance_type == null){
+        sh "error Error con la variable region"
+    }else{
+        sh "echo Region selecionada: $REGION"
+        sh "echo Tipo de instancia selecionada: $INSTANCE_TYPE"
     }
 }
